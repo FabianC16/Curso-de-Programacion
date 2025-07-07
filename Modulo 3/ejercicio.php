6642785845e37dbed85760a9c791dcb83f1eacfd
FabianC16 <fabianomana1615@gmail.com>
2025-07-07 14:18:40 -0400

    h

---
+++ Modulo 3/ejercicio.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+$numero1 = 10;
+$numero2 = 15;
+
+echo "Suma: " . ($numero1 + $numero2) . "\n";          
+echo "Resta: " . ($numero1 - $numero2) . "\n";         
+echo "Multiplicación: " . ($numero1 * $numero2) . "\n"; 
+echo "División: " . ($numero1 / $numero2) . "\n";       
+echo "Módulo: " . ($numero1 % $numero2) . "\n\n";         
+
+?>
